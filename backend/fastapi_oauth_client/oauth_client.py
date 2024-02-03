4419ed13a7a9cd20f19c0fba635f04ade7bcbcf5
Connor <kor.connor@gmail.com>
2024-02-03 17:10:32 +0900

    로그인 버그 수정
    1. 비동기 다 걷어내고 새로짬
    2. object 넘어오는것 return 처리

--- backend/fastapi_oauth_client/oauth_client.py
+++ backend/fastapi_oauth_client/oauth_client.py
@@ -1,8 +1,10 @@
 from urllib import parse
+import requests
 from typing import Dict, Optional
 import aiohttp
 import ssl
 import certifi
+from backend.core.config.env import env
 
 from .exceptions import InvalidAuthorizationCode, InvalidToken
 
@@ -36,7 +38,7 @@ class OAuthClient:
             async with session.get(url, headers=headers) as resp:
                 return None if resp.status != 200 else await resp.json()
 
-    async def _request_post_to(self, url, payload=None) -> Optional[Dict]:
+    async def _request_post_to(self, url, payload=None) -> Optional[dict]:
         conn = self._get_connector_for_ssl()
         async with aiohttp.ClientSession(connector=conn) as session:
             async with session.post(url, data=payload) as resp:
@@ -53,22 +55,40 @@ class OAuthClient:
 
         return f"{self._authentication_uri}/authorize?{query_param}"
 
-    async def get_tokens(self, code: str, state: str) -> Dict:
-        tokens = await self._request_post_to(
+    def get_token_from_kakao(self, code: str, state: str|None) -> str:
+        url = "https://kauth.kakao.com/oauth/token"
+        cid = env.KAKAO_CLIENT_ID
+        cs = env.KAKAO_CLIENT_SECRET_ID
+        payload = {
+            "client_id": cid,
+            "client_secret": cs,
+            "grant_type": "authorization_code",
+            "code": code,
+            # "state": '',
+        }
+        res = requests.post(url, data=payload,headers={'Content-Type': 'application/x-www-form-urlencoded'})
+        t = res.text
+        a = res.json()
+        ac = a.get('access_token')
+
+        return ac
+
+    def get_tokens(self, code: str, state: str) -> dict:
+        tokens = self._request_post_to(
             url=f"{self._authentication_uri}/token",
             payload={
                 "client_id": self._client_id,
                 "client_secret": self._client_secret_id,
                 "grant_type": "authorization_code",
                 "code": code,
-                "state": state,
+                # "state": state,
             },
         )
-        if tokens is None:
-            raise InvalidAuthorizationCode
+        # if tokens is None:
+        #     raise InvalidAuthorizationCode
 
-        if tokens.get("access_token") is None or tokens.get("refresh_token") is None:
-            raise InvalidAuthorizationCode
+        # if tokens.get("access_token") is None or tokens.get("refresh_token") is None:
+        #     raise InvalidAuthorizationCode
 
         return tokens
 
@@ -86,12 +106,16 @@ class OAuthClient:
             raise InvalidToken
         return tokens
 
-    async def get_user_info(self, access_token: str) -> Dict:
+    def get_user_info(self, access_token: str) -> dict:
         headers = {self._header_name: f"{self._header_type} {access_token}"}
-        user_info = await self._request_get_to(url=self._resource_uri, headers=headers)
-        if user_info is None:
-            raise InvalidToken
-        return user_info
+        url=self._resource_uri
+        res = requests.get(url,headers=headers)
+        print(f'res;{res}')
+        # self._request_get_to(url=self._resource_uri, headers=headers)
+        # if user_info is None:
+        #     raise InvalidToken
+        # return user_info
+        return res.json()
 
     async def is_authenticated(self, access_token: str) -> bool:
         headers = {self._header_name: f"{self._header_type} {access_token}"}
